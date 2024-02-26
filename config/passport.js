c4c06b73eedf8285266b3c51873c419f1b8f7e87
simon01092024 <simonkudus510@gmail.com>
2024-02-26 14:06:52 -0800

    set-up passport

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,72 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-//Require your User Model here!
+// File where we can configure our passport strategies (login in with google, login with facebook, etc.... are all strategies)
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const UserModel = require("../models/user");
 
-// configuring Passport!
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
+// this function middleware will be called everytime a USER LOG's in
+passport.use(
+  new GoogleStrategy(
+    // Configuration object, we need to tell google its our app!
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    // The verify callback function (Gets called everytime a user logs in!)
+    async function (accessToken, refreshToken, profile, cb) {
+      //< - information we want is in the profile object
+      // a user has logged in with OAuth...
 
+      // Search the database for users profile
+      // if we find a user with a profile.id that matches a users googleId
+
+      let user = await UserModel.findOne({ googleId: profile.id });
+      // if no user is found user would be undefined
+      if (user) return cb(null, user)
+      // that means the users exists, pass the users information to the next
+      // function in the middleware chain
+      // cb(err, dataYouWantToPassToNextMiddleware)
+
+      // if the user doesn't exist
+      // create the user
+      // then pass the users information to the next function in the middleware 
+      // chain
+      // cb (callback) function signature, cb(error, SuccessfulDataYouWantToPassAlong) in our case successful data will the be users document from mongodb
+      try {
+        user = await UserModel.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value
+        })
+
+        cb(null, user)  // pass the created user to next 
+        // place in middle ware chain
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+// this function is called after the verify callback above, 2nd function called after the user logs
+// in, and its job is to take the userDocument (user) and add the id of the user to the session cookie
+
+passport.serializeUser(function (user, cb) { // user is the userDoc from the function above
+  cb(null, user._id)
+});
+
+// on every http request when the user is logged in, we need to open the cookie, get the user mongodb._id
+// add attach the users document to req.user, so our controller functions know who is making the http request
+passport.deserializeUser(async function (userId, cb) {
+  try {
+    const userDoc = await UserModel.findById(userId)
+    cb(null, userDoc); // this line attaches the userDoc to req.user
+    // req.user = userDoc
+  } catch (err) {
+    cb(err)
   }
-));
-
-passport.serializeUser(function(user, cb) {
-  
-});
-
-passport.deserializeUser(function(id, cb) {
-
-  // Find your User, using your model, and then call cb(err, whateverYourUserIsCalled)
-  // When you call this done function passport assigns the user document to req.user, which will 
-  // be availible in every Single controller function, so you always know the logged in user
-
-});
+})
 
 
 
